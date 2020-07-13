6d8a2bb776509d2a40d79d9d23647a7f32986bd4
jovackbud <jovack123456789@gmail.com>
2020-07-14 08:01:43 +1000

    challenge completed

--- print_num_of_occurence.py
+++ print_num_of_occurence.py
@@ -4,5 +4,8 @@ while n < 7:
     new = int(input('Enter a number here:'))
     numbers.append(new)
     n += 1
-else:
-    print(numbers)
+print(numbers)
+for num in numbers:
+    a = num
+    b = numbers.count(num)
+    print('The number {} appeared {} time(s)'.format(a, b))
